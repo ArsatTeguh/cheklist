5feb51b12f57a2f5d5e729a24b58457e57cfed60
unknown <arsyatteguh@gmail.com>
2022-05-10 17:24:18 +0700

    fix

--- src/Component/Chekclist/index.jsx
+++ src/Component/Chekclist/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {TextField} from "@mui/material";
-import { useNavigate } from "react-router-dom";
 // import "./userCss/Login.css";
 import { Container, Button } from "react-bootstrap";
 
 const Cheklist = () => {
-  const navigate = useNavigate();
   const [token, setToken] = useState([]);
   const [tambahData, setTambahData] = React.useState({
     name: "",
@@ -16,8 +14,7 @@ const Cheklist = () => {
     setToken(
       "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W119.i2OVQdxr08dmIqwP7cWOJk5Ye4fySFUqofl-w6FKbm4EwXTStfm0u-sGhDvDVUqNG8Cc7STtUJlawVAP057Jlg"
     );
-    const response = await axiosJwt.get(
-      "http://94.74.86.174:8080/api/checklist",
+    const response = await axios.get("http://94.74.86.174:8080/api/checklist",
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -27,9 +24,6 @@ const Cheklist = () => {
     console.log(response.data);
   };
 
-  // cara penggunaan axios interceptors
-  const axiosJwt = axios.create();
-
   const handler = (e) => {
     const newDataUser = { ...tambahData };
     newDataUser[e.target.name] = e.target.value;
@@ -37,25 +31,17 @@ const Cheklist = () => {
   };
 
   const TambahData = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("http://94.74.86.174:8080/api/checklist", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        name: tambahData.name,
-      });
-      console.log('berhasil nambah data')
-    } catch (e) {
-      if (e.response) {
-        console.log(e);
-      }
+    const response = await axios.post("http://94.74.86.174:8080/api/checklist",
+    {
+      name: tambahData.name,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      
     }
+  );
+  console.log(response.data);
   };
-
-  // useEffect(() => {
-  //   userLogin()
-  // },[]);
 
   return (
     <>
@@ -63,11 +49,6 @@ const Cheklist = () => {
         <Button onClick={getData} className="mt-3" variant="success">
           Get data
         </Button>
-        {/* <h1>Selamat Datang, {nama}</h1> */}
-
-        {/* <Button onClick={Logout} className="mt-3" variant="warning">
-          Log Out
-        </Button> */}
         <Button onClick={TambahData} className="mt-3" variant="warning">
           tambah data
         </Button>
